42ce7b106e6f3df377a51400f83e272b3587d71d
olgaridzel <olgaridzel@yandex.ru>
2022-07-25 10:07:24 +0300

    clean local dir

---
+++ materials/ptfe/spa/do_all.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+
+energies = [50, 100, 150, 200, 250, 500, 700, 900]
+tey = []
+sey = []
+
+for energy in energies:
+    print(energy)
+    os.system("mast_sey -e {} -m 10000 -ins > ttt".format(energy))
+    res = np.loadtxt('ttt', comments='#')
+    tey.append(res[1])
+    sey.append(res[2])
+
+data = np.column_stack([energies, tey, sey])
+np.savetxt('results', data, fmt=['%d','%.2f','%.2f'])
